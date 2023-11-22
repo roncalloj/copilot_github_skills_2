313024c043a1b86a593b3142d5ce5df45ed3b2a1
roncalloj <107959215+roncalloj@users.noreply.github.com>
2023-11-22 01:03:00 +0000

    Second exercise of copilot

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Juan Camilo",
+  title: "Full Stack Developer",
+  email: "roncalloj@example.com",
+  gitHub: "roncalloj",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
